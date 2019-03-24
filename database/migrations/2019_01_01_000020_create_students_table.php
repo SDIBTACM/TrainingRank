9aaf6306fc27ee65ac8ff69529afec6bc5f7fc2c
boxjan <i@boxjan.li>
2019-03-24 16:07:38 +0800

    hot fixed

--- database/migrations/2019_01_01_000020_create_students_table.php
+++ database/migrations/2019_01_01_000020_create_students_table.php
@@ -19,7 +19,7 @@ class CreateStudentsTable extends Migration
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->string('name', 16)->index();
-            $table->bigIncrements('student_id')->index();
+            $table->bigIntege('student_id')->index();
             $table->integer('group')->default(0)->index();
             $table->tinyInteger('is_show')->default(0)->index();
         });
