7ce9f2847023f7a181e8b276b19bf51baaa5538a
boxjan <i@boxjan.li>
2019-03-25 19:57:11 +0800

    fixed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
             <header>
 
                 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ route('home') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
